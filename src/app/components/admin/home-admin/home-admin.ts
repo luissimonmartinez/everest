783df49f66452f07ffd105316046e4f319915820
Luis Simon Martínez <simonmartinez.luis@gmail.com>
2026-02-28 16:39:25 -0500

    fix show apis

--- src/app/components/admin/home-admin/home-admin.ts
+++ src/app/components/admin/home-admin/home-admin.ts
@@ -23,27 +23,3 @@ export class HomeAdmin {
   }
 }
 
-
-/**
- * // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCJrYh-aMoSeEH9H9wsdiJWiNPGtPNrhkE",
-  authDomain: "portafolio-75332.firebaseapp.com",
-  projectId: "portafolio-75332",
-  storageBucket: "portafolio-75332.firebasestorage.app",
-  messagingSenderId: "777612773520",
-  appId: "1:777612773520:web:c2eb810a3aed0c76b7f015",
-  measurementId: "G-YLD9PD2N0X"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
- */
