0448c9d784ea56988dc04544ca6ef08fff00a387
Luis Simon Martínez <simonmartinez.luis@gmail.com>
2026-03-01 12:15:12 -0500

    fix errors

--- src/app/components/admin/home-admin/home-admin.ts
+++ src/app/components/admin/home-admin/home-admin.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Firebase } from '../../../services/firebase';
+import { log } from 'firebase/firestore/pipelines';
 
 @Component({
   selector: 'app-home-admin',
@@ -7,14 +9,29 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './home-admin.html',
   styleUrls: ['./home-admin.scss'],
 })
-export class HomeAdmin {
+export class HomeAdmin implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor() {
+  constructor(private readonly firebase: Firebase) {
     this.formGroup = new FormGroup({
       title: new FormControl('', [Validators.maxLength(24)]),
       subTitle: new FormControl('', [Validators.maxLength(24)])
+    });
+  }
+
+  ngOnInit() {
+    console.log('HOLA');
+    this.firebase.getDataHome().then(data => {
+      console.log('DATA:', data);
+      if (data.length > 0) {
+        const { title, subTitle } = data[0];
+        this.formGroup.patchValue({ title, subTitle });
+      } else {
+        console.log('No hay datos en data-home');
+      }
+    }).catch(err => {
+      console.error('Error al obtener data-home:', err);
     });
   }
 
@@ -22,4 +39,3 @@ export class HomeAdmin {
     console.log(this.formGroup.value);
   }
 }
-
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAHEoVrb3fYn0hK7-eNOIVE6pvzrKsXI1w",
+    authDomain: "etnica-restaurant.firebaseapp.com",
+    projectId: "etnica-restaurant",
+    storageBucket: "etnica-restaurant.firebasestorage.app",
+    messagingSenderId: "318976755043",
+    appId: "1:318976755043:web:1512046dde11dcbd207f56"
+  }
+};
